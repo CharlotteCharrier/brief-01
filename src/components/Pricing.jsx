02292d71abcdc1a2f39563bd7a1fa02c8bd4b379
CHARRIER Charlotte <charlotte.charrier@maif.fr>
2024-02-01 09:36:29 +0100

    created css file for Pricing component

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,27 +1,28 @@
 import React from "react";
+import '../design/Pricing.css';
 
 export default function Pricing() {
     return (
-        <section id="pricing" className="feature">
+        <section id="features" className="features">
             <h2>Tarifs</h2>
             <div className="feature-container">
                 <div className="feature">
                     <h3 className="time">2 ans</h3>
                     <p className="price">150€</p>
                 </div>
-                <div>
+                <div className="feature">
                     <h3 className="time">1 an</h3>
                     <p className="price">90€</p>
                 </div>
-                <div>
+                <div className="feature">
                     <h3 className="time">6 mois</h3>
                     <p className="price">50€</p>
                 </div>
-                <div>
+                <div className="feature">
                     <h3 className="time">3 mois</h3>
                     <p className="price">40€</p>
                 </div>
-                <div>
+                <div className="feature">
                     <h3 className="time">1 mois</h3>
                     <p className="price">10€</p>
                 </div>
