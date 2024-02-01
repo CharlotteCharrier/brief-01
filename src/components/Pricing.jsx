81bd57f5b278c18e4ca7db3aabf1bc22d5ecdcc5
CHARRIER Charlotte <charlotte.charrier@maif.fr>
2024-02-01 14:18:25 +0100

    combined features.css and pricing.css into section.css

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,29 +1,29 @@
 import React from "react";
-import '../design/Pricing.css';
+import '../design/Section.css';
 
 export default function Pricing() {
 
     return (
-        <section id="pricing" className="features">
+        <section id="pricing" className="pricings">
             <h2>Tarifs</h2>
-            <div className="feature-container">
-                <div className="feature">
+            <div className="pricing-container">
+                <div className="pricing">
                     <h3 className="time">2 ans</h3>
                     <p className="price">150€</p>
                 </div>
-                <div className="feature">
+                <div className="pricing">
                     <h3 className="time">1 an</h3>
                     <p className="price">90€</p>
                 </div>
-                <div className="feature">
+                <div className="pricing">
                     <h3 className="time">6 mois</h3>
                     <p className="price">50€</p>
                 </div>
-                <div className="feature">
+                <div className="pricing">
                     <h3 className="time">3 mois</h3>
                     <p className="price">40€</p>
                 </div>
-                <div className="feature">
+                <div className="pricing">
                     <h3 className="time">1 mois</h3>
                     <p className="price">10€</p>
                 </div>
